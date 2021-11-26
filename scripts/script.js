6c8aaa647638c5aee14d3980de9d659099b7626b
romka-best <romka.best2016@yandex.ru>
2021-11-26 23:53:29 +0300

    refactor: Сверстать модальные окна как отдельные div-ы, а не template
    В темплейт теге необходимо реализовать только карточку, все модальные окна должны быть сверстаны отдельно.

--- scripts/script.js
+++ scripts/script.js
@@ -1,14 +1,14 @@
 "use strict";
-const page = document.querySelector('.page');
-
 const profileEditButton = document.querySelector('.profile__edit-button');
 const postAddButton = document.querySelector('.profile__add-button');
 
 const postTemplate = document.querySelector('#post').content;
 const postsList = document.querySelector('.posts-list');
 
-const popup = document.querySelector('.popup-with-image');
-const closeButton = document.querySelector('.popup__close-button');
+const popupEditProfile = document.querySelector('.popup_type_edit-profile');
+const popupAddPost = document.querySelector('.popup_type_add-post');
+
+const popupWithImage = document.querySelector('.popup-with-image');
 const popupImage = document.querySelector('.popup-with-image__image');
 const popupCaption = document.querySelector('.popup-with-image__figcaption');
 
@@ -39,6 +39,10 @@ const initialCards = [
   }
 ];
 
+setPopup(popupEditProfile);
+setPopup(popupAddPost);
+setPopup(popupWithImage);
+
 function changeReactionPost(event) {
   const reactionPressed = event.target;
   reactionPressed.classList.toggle('post__reaction_active');
@@ -52,79 +56,75 @@ function deletePost(event) {
 function openPopupWithImage(event) {
   popupImage.src = event.target.src;
   popupCaption.textContent = event.target.nextElementSibling.children[0].textContent;
-  popup.style = 'transition: visibility .5s, opacity .5s linear;';
-  popup.classList.add('popup_opened');
+  popupWithImage.style = 'transition: visibility .5s, opacity .5s linear;';
+  popupWithImage.classList.add('popup_opened');
 }
 
-function closePopupWithImage() {
-  popup.classList.remove('popup_opened');
-}
-
-function createPopup(popupTitle, popupNameForm, input1Name, input1Placeholder, input2Name, input2Placeholder) {
-  const popupTemplate = document.querySelector('#popup').content;
-  const popup = popupTemplate.querySelector('.popup').cloneNode(true);
-
-  popup.querySelector('.popup__title').textContent = popupTitle;
-  popup.querySelector('.popup__form').name = popupNameForm;
-
-  const input1 = popup.querySelectorAll('.popup__input-field')[0];
-  const input2 = popup.querySelectorAll('.popup__input-field')[1];
-  input1.name = input1Name;
-  input1.placeholder = input1Placeholder;
-  input2.name = input2Name;
-  input2.placeholder = input2Placeholder;
-
+function setPopup(popup) {
   const popupCloseButton = popup.querySelector('.popup__close-button');
   const popupForm = popup.querySelector('.popup__form');
+  const input1 = popup.querySelectorAll('.popup__input-field')[0];
+  const input2 = popup.querySelectorAll('.popup__input-field')[1];
 
   const name = document.querySelector('.profile__name');
   const description = document.querySelector('.profile__description');
 
-  if (popupNameForm === 'edit_profile') {
+  if (popupForm !== null && popupForm.getAttribute('name') === 'edit_profile') {
     input1.value = name.textContent;
     input2.value = description.textContent;
+  }
+  if (popupForm !== null &&
+    (popupForm.getAttribute('name') === 'add_post' || popupForm.getAttribute('name') === 'edit_profile')) {
+    popup.style = "transition: visibility .5s, opacity .5s linear;"
   }
 
   popupCloseButton.addEventListener('click', () => {
     popup.classList.remove('popup_opened');
   })
 
-  popupForm.addEventListener('invalid', (event) => {
-    event.target.classList.add('popup__input-field_type_warning');
-  }, true);
+  if (popupForm !== null) {
+    popupForm.addEventListener('invalid', (event) => {
+      event.target.classList.add('popup__input-field_type_warning');
+    }, true);
 
-  input1.addEventListener('keypress', () => {
-    changeTypeInput(input1)
-  });
-  input2.addEventListener('keypress', () => {
-    changeTypeInput(input2)
-  })
+    popupForm.addEventListener('submit', (event) => {
+      event.preventDefault();
 
-  popupForm.addEventListener('submit', (event) => {
-    event.preventDefault();
-    input1.classList.remove('popup__input-field_type_warning');
-    input2.classList.remove('popup__input-field_type_warning');
-    const newDataFromInput1 = popup.querySelectorAll('.popup__input-field')[0].value;
-    const newDataFromInput2 = popup.querySelectorAll('.popup__input-field')[1].value;
-    if (popupNameForm === 'edit_profile') {
-      name.textContent = newDataFromInput1;
-      description.textContent = newDataFromInput2;
-    } else if (popupNameForm === 'add_post') {
-      const postElement = postTemplate.querySelector('.post').cloneNode(true);
-      const postsList = document.querySelector('.posts-list');
+      input1.classList.remove('popup__input-field_type_warning');
+      input2.classList.remove('popup__input-field_type_warning');
 
-      postElement.querySelector('.post__title').textContent = newDataFromInput1;
-      postElement.querySelector('.post__photo').src = newDataFromInput2;
-      postElement.querySelector('.post__reaction').addEventListener('click', changeReactionPost);
-      postElement.querySelector('.post__delete').addEventListener('click', deletePost);
-      postElement.querySelector('.post__photo').addEventListener('click', openPopupWithImage);
+      const newDataFromInput1 = popup.querySelectorAll('.popup__input-field')[0];
+      const newDataFromInput2 = popup.querySelectorAll('.popup__input-field')[1];
 
-      postsList.prepend(postElement);
-    }
-    popup.classList.remove('popup_opened');
-  });
+      if (popupForm.getAttribute('name') === 'edit_profile') {
+        name.textContent = newDataFromInput1.value;
+        description.textContent = newDataFromInput2.value;
+      } else if (popupForm.getAttribute('name') === 'add_post') {
+        const postElement = postTemplate.querySelector('.post').cloneNode(true);
 
-  return popup;
+        postElement.querySelector('.post__title').textContent = newDataFromInput1.value;
+        postElement.querySelector('.post__photo').src = newDataFromInput2.value;
+        postElement.querySelector('.post__reaction').addEventListener('click', changeReactionPost);
+        postElement.querySelector('.post__delete').addEventListener('click', deletePost);
+        postElement.querySelector('.post__photo').addEventListener('click', openPopupWithImage);
+
+        postsList.prepend(postElement);
+        newDataFromInput1.value = '';
+        newDataFromInput2.value = '';
+      }
+
+      popup.classList.remove('popup_opened');
+    });
+  }
+
+  if (input1 !== undefined && input2 !== undefined) {
+    input1.addEventListener('keypress', () => {
+      changeTypeInput(input1)
+    });
+    input2.addEventListener('keypress', () => {
+      changeTypeInput(input2)
+    })
+  }
 }
 
 function changeTypeInput(input) {
@@ -145,25 +145,13 @@ initialCards.forEach((card) => {
 
 
 profileEditButton.addEventListener('click', () => {
-  const popupEditProfile = createPopup('Редактировать профиль', 'edit_profile',
-    'name', 'Ваше имя', 'description', 'Ваша деятельность')
-
-  page.append(popupEditProfile);
-
   setTimeout(() => {
     popupEditProfile.classList.add('popup_opened');
   });
 })
 
 postAddButton.addEventListener('click', () => {
-  const popupAddPost = createPopup('Новое место', 'add_post',
-    'name', 'Название', 'url', 'Ссылка на картинку')
-
-  page.append(popupAddPost);
-
   setTimeout(() => {
     popupAddPost.classList.add('popup_opened');
   });
 })
-
-closeButton.addEventListener('click', closePopupWithImage);
